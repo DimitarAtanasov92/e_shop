d70a7314cf38b050e148096b7339b22fdf110148
DimitarAtanasov92 <141042344+DimitarAtanasov92@users.noreply.github.com>
2024-01-09 23:54:40 +0200

    login page

--- e_shop/e_shop_base/urls.py
+++ e_shop/e_shop_base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from e_shop.e_shop_base.views import BaseView, LogoutUserView
+from e_shop.e_shop_base.views import BaseView, LogoutUserView, LoginUserView
 
 urlpatterns = [
     path("", BaseView.as_view(), name="index"),
-    path("logout/", LogoutUserView.as_view(), name="logout")
+    path("logout/", LogoutUserView.as_view(), name="logout"),
+    path("login/", LoginUserView.as_view(), name="login_user"),
 ]
