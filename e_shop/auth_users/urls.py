bc46307d76e16e4d6179ac36ca7f451eccffea4c
DimitarAtanasov92 <141042344+DimitarAtanasov92@users.noreply.github.com>
2024-01-11 23:56:37 +0200

    new page

--- e_shop/auth_users/urls.py
+++ e_shop/auth_users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from e_shop.auth_users.views import RegisterUserView, LogoutUserView, LoginUserView, ProfileDetailView
+from e_shop.auth_users.views import RegisterUserView, LogoutUserView, LoginUserView, ProfileDetailView, \
+    ProfileDeleteView, ProfileUpdateView
 
 urlpatterns = [
     path("registration/", RegisterUserView.as_view(), name="registration"),
     path("logout/", LogoutUserView.as_view(), name="logout"),
     path("login/", LoginUserView.as_view(), name="login_user"),
     path("profile-details/<int:pk>", ProfileDetailView.as_view(), name="profile_detail"),
+    path("profile-delete/<int:pk>", ProfileDeleteView.as_view(), name="profile_delete"),
+    path("profile-edit/<int:pk>", ProfileUpdateView.as_view(), name="profile_edit"),
 ]
